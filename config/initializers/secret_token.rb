cdfd2176215771f1bc4420dfe2ebe4941ea41a64
L Lumbreras <llumbreras@gmail.com>
2013-12-01 07:13:33 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TlPrecourseApp::Application.config.secret_key_base = '9923781cf5b2c5e800654016f4cd1731223cc96aed3553b27d8f739778e7a0bfff5c638b3d388503990d48d772f517763ff7826cad21e55d7540702b7fee1475'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TlPrecourseApp::Application.config.session_store :cookie_store, key: '_tl-precourse-app_session'
